b6ec682a50d47bd657942d740e197ef2da90f836
Andrew Luca <thendrluca@gmail.com>
2019-03-09 12:36:23 +0200

    chore: add gh-pages deploy script

--- src/app.component.tsx
+++ src/app.component.tsx
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.tsx</code> and save to reload.
+            Edit <code>src/App.tsx</code> and save to deploy.
           </p>
           <a
             className="App-link"
