619d4092a3832a0b6a655e25207e3d156af7bc3f
Andrew Luca <iamandrewluca@users.noreply.github.com>
2019-03-11 22:57:17 +0200

    feat: add basic osm map (#4)

--- src/app.component.spec.tsx
+++ src/app.component.spec.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './app.component'
+import { AppComponent } from './app.component'
 
 it('renders without crashing', () => {
   const div = document.createElement('div')
-  ReactDOM.render(<App />, div)
+  ReactDOM.render(<AppComponent />, div)
   ReactDOM.unmountComponentAtNode(div)
 })
