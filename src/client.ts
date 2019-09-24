2eae159e3ae3fedc33e19f59d1661d078e2a3b18
Andrew Luca <thendrluca@gmail.com>
2019-09-24 08:25:11 +0300

    fix: use webstomp URL instead of ws

--- src/client.ts
+++ src/client.ts
@@ -1,23 +1,17 @@
-import { client, Subscription } from 'webstomp-client'
+import { over, Subscription } from 'webstomp-client'
+import Sock from 'sockjs-client'
 
 const data = {
   login: 'public_rtec',
   passcode: 'iWillHackItInVisualBasic',
   host: 'nazar',
-  /**
-   * In production this WebSocket URL is not working
-   * Because we use `https` in production we can't connect to a non secure endpoint
-   * Where needs to be a `wss` protocol to work
-   */
-  ws: 'ws://rtec.dekart.com:15674/ws',
-  // this also does not allow a secure connection
-  stomp: 'http://rtec.dekart.com/webstomp',
+  stomp: 'https://rtec.dekart.com/webstomp',
   transport: '/exchange/e_public_rtec_Sho0ohCiephoh2waeM9t/state.transport.*',
   station: '/exchange/e_public_rtec_Sho0ohCiephoh2waeM9t/state.station.*',
 }
 
 export class Client {
-  client = client(data.ws, { debug: false, heartbeat: false })
+  client = over(new Sock(data.stomp), { debug: false, heartbeat: false })
   subscription?: Subscription = undefined
 
   connect() {
