ddf28c9d9b7fb5f88ae333eeac66298b67df2a8d
Roata wăy <name@withheld.md>
2019-09-27 09:15:38 +0200

    Removed the bearing->cartesian conversion
    The data seem to already come as a cartesian angle, rather than
    as a bearing.

--- src/basic-map.tsx
+++ src/basic-map.tsx
@@ -28,7 +28,11 @@ export function BasicMap() {
               key={p.board}
               title={p.board}
               position={p}
-              // rotationAngle={p.direction}
+              // To show an oriented marker, we have to work around a limitation
+              // of leaflet+react. There is no straightforward way of rotating
+              // the marker, so we overcome this by wrapping it in an invisible
+              // container, and then adding another HTML element inside, which
+              // will use the `arrow` class with a `transform: rotate`.
               icon={divIcon({
                 className: 'arrow-container',
                 html:
