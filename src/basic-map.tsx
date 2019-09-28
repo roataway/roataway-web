b58afb895137d7beea8d42ff578152ab044cf458
Roata wăy <name@withheld.md>
2019-09-29 00:43:30 +0200

    Attempt to display route stations and segments

--- src/basic-map.tsx
+++ src/basic-map.tsx
@@ -1,16 +1,55 @@
 import React from 'react'
-import { Map, TileLayer, Viewport, Marker } from 'react-leaflet'
+import {
+  Map,
+  TileLayer,
+  Viewport,
+  Marker,
+  FeatureGroup,
+  Popup,
+  Circle,
+  GeoJSON,
+} from 'react-leaflet'
 import { divIcon } from 'leaflet'
 import 'leaflet/dist/leaflet.css'
 import './style.css'
 import { Positions } from './use-positions'
+
+// import geodata from './data/route_2_full.json';
 
 const state: Viewport = {
   center: [47.0229, 28.8353],
   zoom: 13,
 }
 
+let geodata = {
+  type: 'FeatureCollection',
+  features: [
+    {
+      type: 'Feature',
+      properties: {
+        style: 'stripe',
+        name: 'Loisirs Lavoie et St-Jean-Baptiste',
+      },
+      geometry: {
+        type: 'Polygon',
+        coordinates: [
+          [
+            [-71.729270299794578, 48.010418784700107],
+            [-71.291070323784993, 48.004374022337799],
+            [-71.291070323784993, 47.777183877693901],
+            [-71.729270299794578, 47.786290622064854],
+            [-71.729270299794578, 48.010418784700107],
+          ],
+        ],
+      },
+    },
+  ],
+}
+
+// import('.data/route_1_segments.json')
+
 export function BasicMap() {
+  console.log(geodata)
   return (
     <Map
       id={'map'}
@@ -44,6 +83,21 @@ export function BasicMap() {
           ))
         }
       </Positions>
+      <GeoJSON
+        key="unique"
+        data={geodata}
+        style={() => ({
+          color: '#4a83ec',
+          weight: 0.5,
+          fillColor: '#1a1d62',
+          fillOpacity: 1,
+        })}
+      />
+
+      <FeatureGroup color="purple">
+        <Popup>Popup in FeatureGroup</Popup>
+        <Circle center={[47.0229, 28.8353]} radius={200} />
+      </FeatureGroup>
     </Map>
   )
 }
