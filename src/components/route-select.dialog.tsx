f31971a2b0c5d0a89b2d7ef24f136e5da7fc1971
Andrew Luca <thendrluca@gmail.com>
2019-10-28 18:20:21 +0200

    feat: allow multiple route select from dialog

--- src/components/route-select.dialog.tsx
+++ src/components/route-select.dialog.tsx
@@ -1,20 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Button from '@material-ui/core/Button'
 import Dialog from '@material-ui/core/Dialog'
 import DialogContent from '@material-ui/core/DialogContent'
-import DialogContentText from '@material-ui/core/DialogContentText'
+import DialogActions from '@material-ui/core/DialogActions'
 import DialogTitle from '@material-ui/core/DialogTitle'
 import Slide from '@material-ui/core/Slide'
 import { TransitionProps } from '@material-ui/core/transitions'
 import useMediaQuery from '@material-ui/core/useMediaQuery'
+import FormControlLabel from '@material-ui/core/FormControlLabel'
+import Checkbox from '@material-ui/core/Checkbox'
 import { useTranslation } from 'react-i18next'
 import { routes } from '../shared/routes'
 import { useTheme } from '@material-ui/styles'
 import { Theme } from '@material-ui/core/styles'
+import { makeStyles } from '@material-ui/core'
 
 const Transition = React.forwardRef(function(props: TransitionProps, ref) {
   return <Slide direction="up" ref={ref} {...props} />
 })
+
+const useStyles = makeStyles(theme => ({
+  buttonMargin: {
+    margin: theme.spacing(1) / 2,
+  },
+  routesSpacing: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    justifyContent: 'center',
+    alignContent: 'start',
+  },
+  contentSplit: {
+    display: 'flex',
+    alignContent: 'space-between',
+  },
+  actions: {
+    justifyContent: 'space-between',
+    paddingLeft: theme.spacing(2),
+  },
+}))
 
 type Props = {
   isOpen: boolean
@@ -26,7 +49,9 @@ type Props = {
 export function RouteSelectDialog(props: Props) {
   const { isOpen, setOpen, setSelectedRoutes, selectedRoutes } = props
   const theme = useTheme<Theme>()
+  const classes = useStyles()
   const fullScreen = useMediaQuery(theme.breakpoints.down('xs'))
+  const [selectMultiple, setSelectMultiple] = useState(false)
   const [l10n] = useTranslation()
 
   function selectRoute(id: string) {
@@ -35,8 +60,10 @@ export function RouteSelectDialog(props: Props) {
     } else {
       selectedRoutes.add(id)
     }
-    setSelectedRoutes(selectedRoutes)
-    setOpen(false)
+    setSelectedRoutes(new Set(selectedRoutes))
+    if (!selectMultiple) {
+      setOpen(false)
+    }
   }
 
   return (
@@ -50,19 +77,34 @@ export function RouteSelectDialog(props: Props) {
       <DialogTitle id="alert-dialog-title">
         {l10n('explanation.pickRoute')}
       </DialogTitle>
-      <DialogContent>
-        <DialogContentText id="alert-dialog-description">
-          {routes.map(r => (
-            <Button
-              onClick={() => selectRoute(r.id_upstream)}
-              color="secondary"
-              variant={selectedRoutes.has(r.id_upstream) ? 'outlined' : 'text'}
-              key={r.id_upstream}>
-              {r.name_concise}
-            </Button>
-          ))}
-        </DialogContentText>
+      <DialogContent dividers className={classes.routesSpacing}>
+        {routes.map(r => (
+          <Button
+            className={classes.buttonMargin}
+            onClick={() => selectRoute(r.id_upstream)}
+            color="secondary"
+            variant={selectedRoutes.has(r.id_upstream) ? 'outlined' : 'text'}
+            key={r.id_upstream}>
+            {r.name_concise}
+          </Button>
+        ))}
       </DialogContent>
+      <DialogActions disableSpacing className={classes.actions}>
+        <FormControlLabel
+          label={l10n('label.selectMultiple')}
+          control={
+            <Checkbox
+              checked={selectMultiple}
+              onChange={() => setSelectMultiple(!selectMultiple)}
+              value="selectMultiple"
+              color="primary"
+            />
+          }
+        />
+        <Button color="primary" onClick={() => setOpen(false)}>
+          {l10n('label.close')}
+        </Button>
+      </DialogActions>
     </Dialog>
   )
 }
