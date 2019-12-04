0b5a0142a2488b5d8f68be9f79f50ffa2a1948cc
Andrew Luca <thendrluca@gmail.com>
2019-12-04 19:38:05 +0200

    feat: combine stations markers for all routes

--- src/components/routes-stations.component.tsx
+++ src/components/routes-stations.component.tsx
@@ -1,7 +1,8 @@
-import React from 'react'
-import { GeoJsonObject } from 'geojson'
-import { Icon, marker } from 'leaflet'
+import React, { useState, useEffect, useRef } from 'react'
+import { FeatureCollection, Point, Feature } from 'geojson'
+import { Icon, marker, Layer } from 'leaflet'
 import { GeoJSON } from 'react-leaflet'
+import uniqBy from 'lodash.uniqby'
 import { ErrorBoundary } from '../shared/error-boundary'
 import markerIcon from 'leaflet/dist/images/marker-icon.png'
 import markerShadow from 'leaflet/dist/images/marker-shadow.png'
@@ -14,62 +15,78 @@ const icon = new Icon({
   iconAnchor: [12, 41],
 })
 
-const importStation = (routeId: string) =>
-  import(
-    /* webpackChunkName: "[request]" */
-    `@roataway/infrastructure-data/data/route_${routeId}_stations.json`
-  )
-
 export function RoutesStations({ selectedRoutes }) {
-  const [routesStations, setRoutesStations] = React.useState<{
-    [key: string]: GeoJsonObject
-  }>({})
-
-  React.useEffect(
-    function() {
-      selectedRoutes.forEach((routeId: string) => {
-        if (!routesStations[routeId]) {
-          importStation(routeId)
-            .then(routeStations =>
-              setRoutesStations(rs => ({
-                ...rs,
-                [routeId]: routeStations.default,
-              })),
-            )
-            .catch(console.log)
-        }
-      })
-    },
-    [routesStations, selectedRoutes, selectedRoutes.size],
-  )
+  const { stations, stationsUpdatedCount } = useStationsFeatureCollection(selectedRoutes)
 
   return (
-    <React.Fragment>
-      {Object.entries(routesStations).map(([id, rs]) =>
-        selectedRoutes.has(id) ? (
-          <ErrorBoundary key={`route-stations-${id}`}>
-            <GeoJSON
-              key={`route-stations-${id}`}
-              data={rs!}
-              pointToLayer={(_, latlng) => marker(latlng, { icon })}
-              onEachFeature={(feature, layer) => {
-                // TODO consider using `name:ru` to load the Russian label,
-                // depending on the user-agent's language setting (if the
-                // translation string is available in the JSON)
-                layer.bindPopup(
-                  // some features are directly on properties.*
-                  // some are put inside a tag object properties.tags.*
-                  feature.properties.tags
-                    ? feature.properties.tags.name
-                    : feature.properties.name,
-                )
-              }}
-            />
-          </ErrorBoundary>
-        ) : (
-          undefined
-        ),
-      )}
-    </React.Fragment>
+    <ErrorBoundary>
+      <GeoJSON
+        // GeoJSON does not rerender when `data` is changed
+        // use react `key` that will trigger a rerender everytime key changes
+        key={stationsUpdatedCount}
+        data={stations}
+        pointToLayer={(_, latlng) => marker(latlng, { icon })}
+        onEachFeature={onEachFeature}
+      />
+    </ErrorBoundary>
   )
+}
+
+type StationProperties = {
+  id: number
+  name?: string
+  tags?: {
+    name: string
+  }
+}
+type StationsFeatureCollection = FeatureCollection<Point, StationProperties>
+const emptyFeatureCollection: StationsFeatureCollection = {
+  type: 'FeatureCollection',
+  features: [],
+}
+
+function useStationsFeatureCollection(selectedRoutes: Set<string>) {
+  const [stations, setStations] = useState(emptyFeatureCollection)
+  const stationsUpdatedCount = useRef(0)
+
+  useEffect(
+    function() {
+      // collect all station Promise<StationsFeatureCollection>
+      const collectionsPromises = Array.from(selectedRoutes).map(importStation)
+      // resolve when all are fetched
+      Promise.all(collectionsPromises).then(featureCollections => {
+        // collect all station points from all station JSONs
+        const duplicatedFeatures = featureCollections.flatMap(fc => fc.features)
+        // get unique points
+        const features = uniqBy(duplicatedFeatures, f => f.properties.id)
+        // this is used as a react `key` for rerender
+        stationsUpdatedCount.current++
+        setStations({ type: 'FeatureCollection', features })
+      })
+    },
+    [selectedRoutes, selectedRoutes.size],
+  )
+
+  return { stations, stationsUpdatedCount: stationsUpdatedCount.current }
+}
+
+function onEachFeature(feature: Feature<Point, StationProperties>, layer: Layer): void {
+  // TODO consider using `name:ru` to load the Russian label,
+  // depending on the user-agent's language setting (if the
+  // translation string is available in the JSON)
+  // some features are directly on properties.*
+  // some are put inside a tag object properties.tags.*
+  const name = feature.properties.tags ? feature.properties.tags.name : feature.properties.name
+  if (name) {
+    layer.bindPopup(name)
+  }
+}
+
+function importStation(routeId: string): Promise<StationsFeatureCollection> {
+  return import(`@roataway/infrastructure-data/data/route_${routeId}_stations.json`)
+    .then(m => m.default)
+    .catch(error => {
+      console.error(error)
+      return emptyFeatureCollection
+    })
 }
