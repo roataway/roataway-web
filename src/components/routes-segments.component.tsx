a5a82158f1839ce67f76155a4653bb97ee581385
Andrew Luca <thendrluca@gmail.com>
2019-12-04 23:45:39 +0200

    feat: combine segments lines for all routes

--- src/components/routes-segments.component.tsx
+++ src/components/routes-segments.component.tsx
@@ -1,48 +1,58 @@
-import React from 'react'
-import { GeoJsonObject } from 'geojson'
+import React, { useState, useRef, useEffect } from 'react'
+import { FeatureCollection, LineString } from 'geojson'
 import { GeoJSON } from 'react-leaflet'
 import { ErrorBoundary } from '../shared/error-boundary'
-
-const importSegment = (routeId: string) =>
-  import(
-    /* webpackChunkName: "[request]" */
-    `@roataway/infrastructure-data/data/route_${routeId}_segments.json`
-  )
+import { uniqBy } from 'lodash'
 
 export function RoutesSegments({ selectedRoutes }) {
-  const [routesSegments, setRoutesSegments] = React.useState<{
-    [key: string]: GeoJsonObject
-  }>({})
-
-  React.useEffect(
-    function() {
-      selectedRoutes.forEach((routeId: string) => {
-        if (!routesSegments[routeId]) {
-          importSegment(routeId)
-            .then(routeSegments =>
-              setRoutesSegments(rs => ({
-                ...rs,
-                [routeId]: routeSegments.default,
-              })),
-            )
-            .catch(console.log)
-        }
-      })
-    },
-    [routesSegments, selectedRoutes, selectedRoutes.size],
-  )
+  const { segments, segmentsUpdatedCount } = useSegmentsFeatureCollection(selectedRoutes)
 
   return (
-    <React.Fragment>
-      {Object.entries(routesSegments).map(([id, rs]) =>
-        selectedRoutes.has(id) ? (
-          <ErrorBoundary key={`route-segments-${id}`}>
-            <GeoJSON data={rs!} />
-          </ErrorBoundary>
-        ) : (
-          undefined
-        ),
-      )}
-    </React.Fragment>
+    <ErrorBoundary>
+      <GeoJSON key={segmentsUpdatedCount} data={segments} />
+    </ErrorBoundary>
   )
+}
+
+type SegmentProperties = {
+  id: number
+}
+type SegmentsFeatureCollection = FeatureCollection<LineString, SegmentProperties>
+const emptyFeatureCollection: SegmentsFeatureCollection = {
+  type: 'FeatureCollection',
+  features: [],
+}
+
+function useSegmentsFeatureCollection(selectedRoutes: Set<string>) {
+  const [segments, setSegments] = useState(emptyFeatureCollection)
+  const segmentsUpdatedCount = useRef(0)
+
+  useEffect(
+    function() {
+      // collect all segments Promise<SegmentsFeatureCollection>
+      const collectionsPromises = Array.from(selectedRoutes).map(importSegment)
+      // resolve when all are fetched
+      Promise.all(collectionsPromises).then(featureCollections => {
+        // collect all features from all segment JSONs
+        const duplicatedFeatures = featureCollections.flatMap(fc => fc.features)
+        // get unique features
+        const features = uniqBy(duplicatedFeatures, f => f.properties.id)
+        // this is used as a react `key` for rerender
+        segmentsUpdatedCount.current++
+        setSegments({ type: 'FeatureCollection', features })
+      })
+    },
+    [selectedRoutes, selectedRoutes.size],
+  )
+
+  return { segments, segmentsUpdatedCount: segmentsUpdatedCount.current }
+}
+
+function importSegment(routeId: string): Promise<SegmentsFeatureCollection> {
+  return import(`@roataway/infrastructure-data/data/route_${routeId}_segments.json`)
+    .then(m => m.default)
+    .catch(error => {
+      console.error(error)
+      return emptyFeatureCollection
+    })
 }
