221948091c8a427f5cea1872831a70a680467e00
Roata wăy <name@withheld.md>
2020-01-25 23:57:34 +0100

    Use different map provider
    This one is free, privacy-preserving, but most importantly,
    the map contrast is better, because it is not cluttered with
    various objects that stand out (like hospitals, on the
    previous tiles we used).

--- src/the-map.tsx
+++ src/the-map.tsx
@@ -50,10 +50,9 @@ export function TheMap(props: Props) {
       zoomControl={false}
       viewport={viewport}>
       <TileLayer
-        attribution='&amp;copy <a href="https://osm.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution="<a href='https://wikimediafoundation.org/wiki/Maps_Terms_of_Use'>Wikimedia</a>"
+        url="https://maps.wikimedia.org/osm-intl/{z}/{x}/{y}{r}.png"
       />
-
       {showUserLocation && <UserLocation />}
 
       <ZoomControl position={leftHanded ? 'bottomleft' : 'bottomright'} />
