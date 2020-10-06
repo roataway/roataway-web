4dfa05cb00634b5dc6fdab9ef05ced58bacfe28a
Grigore Budac <budacgrigore2000@gmail.com>
2020-10-06 20:47:50 +0200

    add snackbar to notify the user about the submitted action

--- src/components/provide-feedback.dialog.tsx
+++ src/components/provide-feedback.dialog.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useForm, Controller } from 'react-hook-form'
 import {
   Button,
@@ -8,6 +8,7 @@ import {
   DialogContentText,
   DialogTitle,
   TextField,
+  Snackbar,
 } from '@material-ui/core'
 import { useTranslation } from 'react-i18next'
 
@@ -23,6 +24,7 @@ type FeedbackFormValues = {
 }
 
 export function ProvideFeedbackDialog(props: Props) {
+  const [snackbarMessage, setSnackbarMessage] = useState<string | null>(null)
   const { handleSubmit, control } = useForm<FeedbackFormValues>()
   const { t } = useTranslation()
 
@@ -30,11 +32,11 @@ export function ProvideFeedbackDialog(props: Props) {
     props.setOpen(false)
   }
 
-  function handleSubmitFeedback(data: FeedbackFormValues) {
+  async function handleSubmitFeedback(data: FeedbackFormValues) {
     const { REACT_APP_SENTRY_DSN, REACT_APP_SENTRY_ORGANIZATION_SLUG, REACT_APP_SENTRY_PROJECT_SLUG } = process.env
 
     const body = {
-      event_id: 'user-feedback',
+      event_id: Date.now(),
       name: data.name,
       email: data.email,
       comments: data.content,
@@ -51,66 +53,86 @@ export function ProvideFeedbackDialog(props: Props) {
         body: JSON.stringify(body),
       },
     )
+      .then(() => {
+        setSnackbarMessage(t('message.submitFeedback.success'))
+      })
+      .catch(() => {
+        setSnackbarMessage(t('message.submitFeedback.error'))
+      })
 
     handleClose()
   }
 
+  function handleCloseSnackbar() {
+    setSnackbarMessage('')
+  }
+
   return (
-    <Dialog open={props.isOpen} onClose={handleClose} aria-labelledby="form-dialog-title">
-      <form onSubmit={handleSubmit(handleSubmitFeedback)}>
-        <DialogTitle> {t('label.feedback')}</DialogTitle>
-        <DialogContent>
-          <DialogContentText>{t('label.feedback.description')}</DialogContentText>
-          <Controller
-            as={TextField}
-            control={control}
-            name="name"
-            defaultValue=""
-            variant="outlined"
-            autoFocus
-            label={t('label.name')}
-            type="text"
-            fullWidth
-            margin="normal"
-            required
-          />
+    <>
+      <Dialog open={props.isOpen} onClose={handleClose} aria-labelledby="form-dialog-title">
+        <form onSubmit={handleSubmit(handleSubmitFeedback)}>
+          <DialogTitle> {t('label.feedback')}</DialogTitle>
+          <DialogContent>
+            <DialogContentText>{t('label.feedback.description')}</DialogContentText>
+            <Controller
+              as={TextField}
+              control={control}
+              name="name"
+              defaultValue=""
+              variant="outlined"
+              autoFocus
+              label={t('label.name')}
+              type="text"
+              fullWidth
+              margin="normal"
+              required
+            />
 
-          <Controller
-            as={TextField}
-            control={control}
-            name="email"
-            defaultValue=""
-            variant="outlined"
-            label={t('label.email')}
-            type="email"
-            margin="normal"
-            fullWidth
-            required
-          />
+            <Controller
+              as={TextField}
+              control={control}
+              name="email"
+              defaultValue=""
+              variant="outlined"
+              label={t('label.email')}
+              type="email"
+              margin="normal"
+              fullWidth
+              required
+            />
 
-          <Controller
-            as={TextField}
-            control={control}
-            name="content"
-            defaultValue=""
-            variant="outlined"
-            label={t('label.content')}
-            rows={5}
-            multiline
-            fullWidth
-            margin="normal"
-            required
-          />
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={handleClose} color="primary">
-            {t('button.cancel')}
-          </Button>
-          <Button type="submit" color="primary">
-            {t('button.submit')}
-          </Button>
-        </DialogActions>
-      </form>
-    </Dialog>
+            <Controller
+              as={TextField}
+              control={control}
+              name="content"
+              defaultValue=""
+              variant="outlined"
+              label={t('label.content')}
+              rows={5}
+              multiline
+              fullWidth
+              margin="normal"
+              required
+            />
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={handleClose} color="primary">
+              {t('button.cancel')}
+            </Button>
+            <Button type="submit" color="primary">
+              {t('button.submit')}
+            </Button>
+          </DialogActions>
+        </form>
+      </Dialog>
+
+      <Snackbar
+        anchorOrigin={{ vertical: 'bottom', horizontal: 'left' }}
+        autoHideDuration={3000}
+        open={Boolean(snackbarMessage)}
+        message={snackbarMessage}
+        onClose={handleCloseSnackbar}
+      />
+    </>
   )
 }
