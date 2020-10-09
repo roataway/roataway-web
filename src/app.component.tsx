6dcc25669d20c9367e203facd80cdfbd340c4362
Grigore Budac <budacgrigore2000@gmail.com>
2020-10-09 08:59:30 +0200

    feat: add feedback mechanism (#135)
    * feat: provide a dialog to interact with feedback mechanism
    
    * feat: submit feedback using sentry api
    
    * store client feedback using sentry's user feedback api
    
    * add snackbar to notify the user about the submitted action

--- src/app.component.tsx
+++ src/app.component.tsx
@@ -5,6 +5,7 @@ import { NavigationBarComponent } from './components/navigation-bar.component'
 import { useTranslation } from 'react-i18next'
 import { HudButtons } from './components/hud-buttons.component'
 import { RouteSelectDialog } from './components/route-select.dialog'
+import { ProvideFeedbackDialog } from './components/provide-feedback.dialog'
 import { useDocumentTitle } from './shared/document-title.hook'
 import classes from './app.module.scss'
 
@@ -12,6 +13,7 @@ export function AppComponent() {
   const { t } = useTranslation()
   useDocumentTitle(t('label.title'))
   const [isOpenRouteSelect, setIsOpenRouteSelect] = useState<boolean>(false)
+  const [isOpenProvideFeedback, seIsOpenProvideFeedback] = useState<boolean>(false)
   // Maybe keep it in local storage?
   const [showUserLocation, setShowUserLocation] = useState<number | undefined>(undefined)
   const [firstVisit, setFirstVisit] = useState<boolean>(() => !areStoredRoutesEmpty())
@@ -26,11 +28,15 @@ export function AppComponent() {
 
   const setCurrentUserLocation = useCallback(() => setShowUserLocation(new Date().getTime()), [])
 
+  const handleOpenFeedback = () => {
+    seIsOpenProvideFeedback(true)
+  }
+
   return (
     <div className={classes.root}>
       <CssBaseline />
 
-      <NavigationBarComponent />
+      <NavigationBarComponent onOpenFeedback={handleOpenFeedback} />
 
       <HudButtons
         setCurrentUserLocation={setCurrentUserLocation}
@@ -39,7 +45,7 @@ export function AppComponent() {
       />
 
       <RouteSelectDialog isOpen={isOpenRouteSelect} setOpen={setIsOpenRouteSelect} />
-
+      <ProvideFeedbackDialog isOpen={isOpenProvideFeedback} setOpen={seIsOpenProvideFeedback} />
       <TheMap showUserLocation={showUserLocation} className={classes.map} />
     </div>
   )
---
+++ src/components/provide-feedback.dialog.tsx
@@ -0,0 +1,138 @@
+import React, { useState } from 'react'
+import { useForm, Controller } from 'react-hook-form'
+import {
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+  TextField,
+  Snackbar,
+} from '@material-ui/core'
+import { useTranslation } from 'react-i18next'
+
+type Props = {
+  isOpen: boolean
+  setOpen: (isOpen: boolean) => void
+}
+
+type FeedbackFormValues = {
+  name: string
+  email: string
+  content: string
+}
+
+export function ProvideFeedbackDialog(props: Props) {
+  const [snackbarMessage, setSnackbarMessage] = useState<string | null>(null)
+  const { handleSubmit, control } = useForm<FeedbackFormValues>()
+  const { t } = useTranslation()
+
+  function handleClose() {
+    props.setOpen(false)
+  }
+
+  async function handleSubmitFeedback(data: FeedbackFormValues) {
+    const { REACT_APP_SENTRY_DSN, REACT_APP_SENTRY_ORGANIZATION_SLUG, REACT_APP_SENTRY_PROJECT_SLUG } = process.env
+
+    const body = {
+      event_id: Date.now(),
+      name: data.name,
+      email: data.email,
+      comments: data.content,
+    }
+
+    fetch(
+      `https://sentry.io/api/0/projects/${REACT_APP_SENTRY_ORGANIZATION_SLUG}/${REACT_APP_SENTRY_PROJECT_SLUG}/user-feedback/`,
+      {
+        method: 'POST',
+        headers: {
+          Authorization: `DSN ${REACT_APP_SENTRY_DSN}`,
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(body),
+      },
+    )
+      .then(() => {
+        setSnackbarMessage(t('message.submitFeedback.success'))
+      })
+      .catch(() => {
+        setSnackbarMessage(t('message.submitFeedback.error'))
+      })
+
+    handleClose()
+  }
+
+  function handleCloseSnackbar() {
+    setSnackbarMessage('')
+  }
+
+  return (
+    <>
+      <Dialog open={props.isOpen} onClose={handleClose} aria-labelledby="form-dialog-title">
+        <form onSubmit={handleSubmit(handleSubmitFeedback)}>
+          <DialogTitle> {t('label.feedback')}</DialogTitle>
+          <DialogContent>
+            <DialogContentText>{t('label.feedback.description')}</DialogContentText>
+            <Controller
+              as={TextField}
+              control={control}
+              name="name"
+              defaultValue=""
+              variant="outlined"
+              autoFocus
+              label={t('label.name')}
+              type="text"
+              fullWidth
+              margin="normal"
+              required
+            />
+
+            <Controller
+              as={TextField}
+              control={control}
+              name="email"
+              defaultValue=""
+              variant="outlined"
+              label={t('label.email')}
+              type="email"
+              margin="normal"
+              fullWidth
+              required
+            />
+
+            <Controller
+              as={TextField}
+              control={control}
+              name="content"
+              defaultValue=""
+              variant="outlined"
+              label={t('label.content')}
+              rows={5}
+              multiline
+              fullWidth
+              margin="normal"
+              required
+            />
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={handleClose} color="primary">
+              {t('button.cancel')}
+            </Button>
+            <Button type="submit" color="primary">
+              {t('button.submit')}
+            </Button>
+          </DialogActions>
+        </form>
+      </Dialog>
+
+      <Snackbar
+        anchorOrigin={{ vertical: 'bottom', horizontal: 'left' }}
+        autoHideDuration={3000}
+        open={Boolean(snackbarMessage)}
+        message={snackbarMessage}
+        onClose={handleCloseSnackbar}
+      />
+    </>
+  )
+}
