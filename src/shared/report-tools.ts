c3143b673f7063bfb443e0dbc4934904a5f44f38
Andrew Luca <thendrluca@gmail.com>
2021-01-08 17:44:57 +0200

    fix: add list styles, encode uri component

--- src/shared/report-tools.ts
+++ src/shared/report-tools.ts
@@ -1,19 +1,15 @@
+/**
+ * Produce a mailto link with predefined subject and body.
+ * Use https://mailtolinkgenerator.com for experiments with the structure of such links
+ *
+ * @param to - To whom we send the email
+ * @param subject - Subject of the email
+ * @param body - Message text
+ * @param cc - Optional, carbon copy to send to
+ * @returns The URL ready for use in the HREF attribute of the A tag
+ */
 export function generateMailto(to: string, subject: string, body: string, cc?: string): string {
-  /**
-   * Produce a mailto link with predefined subject and body.
-   *
-   * @param to - To whom we send the email
-   * @param subject - Subject of the email
-   * @param body - Message text
-   * @param cc - Optional, carbon copy to send to
-   * @returns The URL ready for use in the HREF attribute of the A tag
-   */
-
-  // use https://mailtolinkgenerator.com for experiments with the structure of such links
-
-  if (cc) {
-    return `mailto:${to}?cc=${cc}&subject=${encodeURI(subject)}&body=${encodeURI(body)}`
-  }
-
-  return `mailto:${to}?subject=${encodeURI(subject)}&body=${encodeURI(body)}`
+  return cc
+    ? `mailto:${to}?cc=${cc}&subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(body)}`
+    : `mailto:${to}?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(body)}`
 }
