461ef5cdaa8242afabb8ee7a60e29eac2e45a659
Jacobojijo <jacobojijo2017@gmail.com>
2023-10-27 14:23:02 +0300

    add PWA feature

--- src/index.tsx
+++ src/index.tsx
@@ -45,3 +45,17 @@ render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals()
+
+// src/index.js
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker
+      .register('/service-worker.js')
+      .then((registration) => {
+        console.log('Service Worker registered with scope:', registration.scope)
+      })
+      .catch((error) => {
+        console.error('Service Worker registration failed:', error)
+      })
+  })
+}
